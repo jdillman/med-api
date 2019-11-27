40f03f7788de0429e529002151949d4a05f5a6a5
jdillman <jdd619@gmail.com>
2019-11-26 20:01:27 -0800

    relax cors for dev

--- config/application.rb
+++ config/application.rb
@@ -34,6 +34,15 @@ module MedApi
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
 
+    config.public_file_server.enabled = false
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: %i[get post options]
+      end
+    end
+
     config.generators do |g|
       g.orm :active_record, primary_key_type: :uuid
     end
